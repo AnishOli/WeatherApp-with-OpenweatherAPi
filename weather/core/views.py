5f0296036110e95e2ab674133c350197bdf2ac1b
Anish oli <olianish534@gmail.com>
2026-03-17 23:53:53 +0545

    Image api according to city with unsplashapi

--- weather/core/views.py
+++ weather/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import requests
 from decouple import config
+from django.contrib import messages
 # Create your views here.
 def index(request):
     if 'city' in request.POST:
@@ -10,19 +11,47 @@ def index(request):
     API_KEY = config("WEATHER_API_KEY")
     url=f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
     data=requests.get(url).json()
-    temp= data['main']['temp']
-    # temp_celious=round(temp-273.15)
-    desc=data['weather'][0]['description']
-    windsp=data['wind']['speed']
-    humid=data['main']['humidity']
-    pressure=data['main']['pressure']
-    visible=data['visibility']
-    context={'temp':temp,
-             'city':city,
-             'desc':desc,
-             'windsp':windsp,
-             'humid':humid,
-             'pressure':pressure,
-             'visible':visible
-             }
-    return render(request,'core/index.html',context)
+    img_url=f'https://api.unsplash.com/search/photos?query={city}&per_page=1&client_id=AocYclv8-7zt1nkxHU7cN4ZMEXoOoNvgBwIvjMYUufI'
+    response=requests.get(img_url).json()
+  
+    try:
+        temp= data['main']['temp']
+        # temp_celious=round(temp-273.15)
+        desc=data['weather'][0]['description']
+        windsp=data['wind']['speed']
+        humid=data['main']['humidity']
+        pressure=data['main']['pressure']
+        visible=data['visibility']
+        feel=data['main']['feels_like']
+        image= response['results'][0]['urls']['regular']
+        context={'temp':temp,
+                'city':city,
+                'desc':desc,
+                'windsp':windsp,
+                'humid':humid,
+                'pressure':pressure,
+                'visible':visible,
+                'feel':feel,
+                'image':image
+                }
+        return render(request,'core/index.html',context)
+    except:
+        messages.error(request,'No such city!!!')
+        temp= 0
+        # temp_celious=round(temp-273.15)
+        desc="No city"
+        windsp=0
+        humid=0
+        pressure=0
+        visible=0
+        image="No image available"
+        context={'temp':temp,
+                'city':city,
+                'desc':desc,
+                'windsp':windsp,
+                'humid':humid,
+                'pressure':pressure,
+                'visible':visible
+                }
+        return render(request,'core/index.html',context)
+        
